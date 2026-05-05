f6031d19639a6ff17d38cf753ec5165708c2061f
Juan Carlos Prieto <juanprietob@gmail.com>
2026-05-05 11:55:33 -0400

    enh: export model

--- dl/cut_export.py
+++ dl/cut_export.py
@@ -7,4 +7,4 @@ model_fn = "/mnt/GWH/Groups/FAMLI/Shared/C1_ML_Analysis/train_output/Cut/allvsla
 
 model = cut.CutG.load_from_checkpoint(model_fn).eval()
 x = torch.rand(1, 1, 256, 256)
-model.to_torchscript(file_path=model_fn.replace('.ckpt', '_v0.1.pt'), method="trace", example_inputs=x.cuda())
+model.to_torchscript(file_path='/mnt/GWH/Groups/FAMLI/Shared/C1_ML_Analysis/trained_models/cut/all2leltek_v1_epoch=6-val_loss=6.62.pt', method="trace", example_inputs=x.cuda())

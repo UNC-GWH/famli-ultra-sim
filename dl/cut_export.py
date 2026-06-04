43570f13cc8a5fee2df38cb3378ca3940321ca44
Juan Carlos Prieto <juanprietob@gmail.com>
2026-06-03 23:10:30 -0400

    enh: export function

--- dl/cut_export.py
+++ dl/cut_export.py
@@ -3,7 +3,7 @@ from nets import cut
 
 
 
-model_fn = "/mnt/GWH/Groups/FAMLI/Shared/C1_ML_Analysis/train_output/Cut/allvslast/allvsleltek/v0.1/epoch=6-val_loss=6.62.ckpt"
+model_fn = "/mnt/GWH/Groups/FAMLI/Shared/C1_ML_Analysis/train_output/Cut/allvslast/allvsleltek/v0.1/last.ckpt"
 
 model = cut.CutG.load_from_checkpoint(model_fn).eval()
 x = torch.rand(1, 1, 256, 256)
